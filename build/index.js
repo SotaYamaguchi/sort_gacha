ae25bbd3ec6c3d7408190be4388c9ff664979024
Shohei-Japan <kai.entertainment0304@gmail.com>
2021-11-24 18:10:43 +0900

    feat: generateを実行してindex.jsを更新

--- build/index.js
+++ build/index.js
@@ -3,15 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 var discord_js_1 = require("discord.js");
 var client = new discord_js_1.Client();
-client.on('message', function (message) {
-    if (message.author.bot) {
-        return;
-    }
-    if (message.content === 'おはよう') {
-        message.channel.send('どうもどうも');
-    }
-});
-// トークンを発行したアプリケーション名を確認のために表示
+/**
+ * 接続確認のために bot 名を表示
+ */
 client.on('ready', function () {
     if (!client.user) {
         console.error('認証できないよ');
@@ -20,28 +14,40 @@ client.on('ready', function () {
     console.log("Logged in as " + client.user.tag + ".");
 });
 /**
- * チャット欄のコメントを受け取る処理
+ * チャット欄のコメントを受け取ってメンバーリストを返す処理
  */
 client.on('message', function (message) {
     if (!message.guild) {
         console.error('ギルドがないよ');
         return;
     }
-    if (message.content === '!gacha') {
-        var textCH = message.guild.channels.cache.find(function (ch) { return ch.type === 'text'; });
-        if (!textCH) {
-            return;
-        }
-        var textCHMemberNames = textCH.guild.members.cache
-            .filter(function (member) { return !member.user.bot; })
-            .map(function (member) { return member.user.username; });
-        var shuffledMembers = shuffleArray(textCHMemberNames);
-        var joinedMembers = shuffledMembers
-            .map(function (member, index) { return index + 1 + ". " + member; })
-            .join('\n');
-        message.channel.send(joinedMembers);
+    // `!gacha` 以外のコマンドだったら無視
+    if (message.content !== '!gacha') {
+        return;
     }
+    // 現状のチャンネル一覧から daily_scrum チャンネルを取得
+    var voiceChs = client.channels.cache.filter(function (ch) { return ch instanceof discord_js_1.VoiceChannel; });
+    // daily_scrum ボイスチャンネルを特定
+    // TODO: ボイスチャンネルの型を当てたい
+    var targetVoiceCh = voiceChs.find(function (ch) { return ch.id === '908188153792311297'; });
+    if (!targetVoiceCh) {
+        console.error('チャンネルがみっかんないよ');
+        return;
+    }
+    // 参加中の voice チャンネルの参加者全員の名前を取得
+    var voiceCHMemberNames = targetVoiceCh.members
+        .map(function (member) { return member.user.username; });
+    // ランダムに並び替える
+    var shuffledMembers = shuffleArray(voiceCHMemberNames);
+    // リストにしてテキストチャンネルに送信する
+    var joinedMembers = shuffledMembers
+        .map(function (member, index) { return index + 1 + ". " + member; })
+        .join('\n');
+    var sendMessage = joinedMembers || 'ボイスチャンネルのメンバーを取得できなかったよ';
+    // メッセージを返す
+    message.channel.send(sendMessage);
 });
+// 与えられた配列をシャッフルして返す
 var shuffleArray = function (array) {
     for (var i = (array.length - 1); 0 < i; i--) {
         var r = Math.floor(Math.random() * (i + 1));
