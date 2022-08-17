642ed09ba96572eb6e8777010e2be79b45873b89
Toshihiro Kanai <i@miruc.co>
2022-08-17 19:32:03 +0900

    feat: 特定のボイスチャンネルに依存しないように

--- build/index.js
+++ build/index.js
@@ -1,55 +1,92 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 var discord_js_1 = require("discord.js");
-var client = new discord_js_1.Client();
+var client = new discord_js_1.Client({
+    intents: [discord_js_1.GatewayIntentBits.Guilds, discord_js_1.GatewayIntentBits.GuildMessages],
+});
 /**
  * 接続確認のために bot 名を表示
  */
-client.on('ready', function () {
+client.once('ready', function () {
     if (!client.user) {
         console.error('認証できないよ');
         return;
     }
-    console.log("Logged in as " + client.user.tag + ".");
+    console.log("Logged in as ".concat(client.user.tag, "."));
 });
-/**
- * チャット欄のコメントを受け取ってメンバーリストを返す処理
- */
-client.on('message', function (message) {
-    if (!message.guild) {
-        console.error('ギルドがないよ');
-        return;
-    }
-    // `!gacha` 以外のコマンドだったら無視
-    if (message.content !== '!gacha') {
-        return;
-    }
-    // 現状のチャンネル一覧から daily_scrum チャンネルを取得
-    var voiceChs = client.channels.cache.filter(function (ch) { return ch instanceof discord_js_1.VoiceChannel; });
-    // daily_scrum ボイスチャンネルを特定
-    // TODO: ボイスチャンネルの型を当てたい
-    var targetVoiceCh = voiceChs.find(function (ch) { return ch.id === '908188153792311297'; });
-    if (!targetVoiceCh) {
-        console.error('チャンネルがみっかんないよ');
-        return;
-    }
-    // 参加中の voice チャンネルの参加者全員の名前を取得
-    var voiceCHMemberNames = targetVoiceCh.members
-        .map(function (member) { return member.user.username; });
-    // ランダムに並び替える
-    var shuffledMembers = shuffleArray(voiceCHMemberNames);
-    // リストにしてテキストチャンネルに送信する
-    var joinedMembers = shuffledMembers
-        .map(function (member, index) { return index + 1 + ". " + member; })
-        .join('\n');
-    var sendMessage = joinedMembers || 'ボイスチャンネルのメンバーを取得できなかったよ';
-    // メッセージを返す
-    message.channel.send(sendMessage);
-});
+client.on('interactionCreate', function (interaction) { return __awaiter(void 0, void 0, void 0, function () {
+    var targetVoiceCh, voiceCHMemberNames, shuffledMembers, joinedMembers, sendMessage;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!interaction.isChatInputCommand()) {
+                    return [2 /*return*/];
+                }
+                if (interaction.commandName !== 'gacha') {
+                    return [2 /*return*/];
+                }
+                targetVoiceCh = interaction.channel;
+                if (!(targetVoiceCh === null || targetVoiceCh.type !== discord_js_1.ChannelType.GuildVoice)) return [3 /*break*/, 2];
+                return [4 /*yield*/, interaction.reply('ボイスチャンネル以外では使えないよ')];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+            case 2:
+                voiceCHMemberNames = targetVoiceCh.members.map(function (member) { return member.user.username; });
+                shuffledMembers = shuffleArray(voiceCHMemberNames);
+                joinedMembers = shuffledMembers
+                    .map(function (member, index) { return "".concat(index + 1, ". ").concat(member); })
+                    .join('\n');
+                sendMessage = joinedMembers || 'ボイスチャンネルのメンバーを取得できなかったよ';
+                // メッセージを返す
+                return [4 /*yield*/, interaction.reply(sendMessage)];
+            case 3:
+                // メッセージを返す
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
 // 与えられた配列をシャッフルして返す
 var shuffleArray = function (array) {
-    for (var i = (array.length - 1); 0 < i; i--) {
+    for (var i = array.length - 1; 0 < i; i--) {
         var r = Math.floor(Math.random() * (i + 1));
         var tmp = array[i];
         array[i] = array[r];
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 require('dotenv').config()
-import {
-  Client,
+
+import type {
   GuildMember,
-  Message,
+  Interaction,
   VoiceChannel,
-} from "discord.js"
-const client = new Client()
+  VoiceBasedChannel,
+} from 'discord.js'
+import { Client, GatewayIntentBits, ChannelType } from 'discord.js'
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+})
 
 /**
  * 接続確認のために bot 名を表示
  */
-client.on('ready', () => {
+client.once('ready', () => {
   if (!client.user) {
     console.error('認証できないよ')
     return
@@ -18,35 +23,30 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}.`)
 })
 
-/**
- * チャット欄のコメントを受け取ってメンバーリストを返す処理
- */
-client.on('message', (message: Message) => {
-  if (!message.guild) {
-    console.error('ギルドがないよ')
+client.on('interactionCreate', async (interaction: Interaction) => {
+  if (!interaction.isChatInputCommand()) {
     return
   }
 
-  // `!gacha` 以外のコマンドだったら無視
-  if (message.content !== '!gacha') {
+  if (interaction.commandName !== 'gacha') {
     return
   }
-
-  // 現状のチャンネル一覧から目的のチャンネルを取得
-  const voiceChs = client.channels.cache.filter(ch => ch instanceof VoiceChannel)
 
   // 目的のボイスチャンネルを特定
-  // TODO: ボイスチャンネルの型を当てたい
-  const targetVoiceCh: any = voiceChs.find((ch: any) => ch.id === '908188153792311297')
+  const targetVoiceCh = interaction.channel
 
-  if (!targetVoiceCh) {
-    console.error('チャンネルがみっかんないよ')
+  // targetVoiceCh の型がおかしく、ボイスチャンネルが入ってくることが想定されていないため ts-ignore する.
+  // 動作に支障はない.
+  // @ts-ignore
+  if (targetVoiceCh === null || targetVoiceCh.type !== ChannelType.GuildVoice) {
+    await interaction.reply('ボイスチャンネル以外では使えないよ')
     return
   }
 
   // 参加中の voice チャンネルの参加者全員の名前を取得
-  const voiceCHMemberNames: string[] = targetVoiceCh.members
-    .map((member: GuildMember) => member.user.username)
+  const voiceCHMemberNames: string[] = (
+    targetVoiceCh as VoiceChannel
+  ).members.map((member: GuildMember) => member.user.username)
 
   // ランダムに並び替える
   const shuffledMembers = shuffleArray(voiceCHMemberNames)
@@ -56,18 +56,19 @@ client.on('message', (message: Message) => {
     .map((member: string, index: number) => `${index + 1}. ${member}`)
     .join('\n')
 
-  const sendMessage = joinedMembers || 'ボイスチャンネルのメンバーを取得できなかったよ'
+  const sendMessage =
+    joinedMembers || 'ボイスチャンネルのメンバーを取得できなかったよ'
 
   // メッセージを返す
-  message.channel.send(sendMessage)
+  await interaction.reply(sendMessage)
 })
 
 // 与えられた配列をシャッフルして返す
 const shuffleArray = (array: string[]): string[] => {
-  for(let i = (array.length - 1); 0 < i; i--){
-    var r = Math.floor(Math.random() * (i + 1))
+  for (let i = array.length - 1; 0 < i; i--) {
+    const r = Math.floor(Math.random() * (i + 1))
 
-    var tmp = array[i]
+    const tmp = array[i]
     array[i] = array[r]
     array[r] = tmp
   }
